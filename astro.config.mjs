32211dce76d5dae0c59f72a6a777e17321847cc1
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-07-01 20:03:47 -0400

    set tailwind3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  integrations: [tailwind()],
   output: 'server',
   adapter: node({ mode: 'standalone' }),
 });
