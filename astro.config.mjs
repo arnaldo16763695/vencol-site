029da16dd3bd2c4396ca1677fdb6591464661904
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-08-04 20:16:26 -0400

    test return config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-import vercel from "@astrojs/vercel";
 
-const isVercel = process.env.VERCEL === "1";
 export default defineConfig({
   integrations: [tailwind()],
-  output: isVercel ? "server" : "server", // puedes mantenerlo 'server' en ambos casos
-  adapter: isVercel
-    ? vercel() // Si está en Vercel, usa el adaptador de Vercel
-    : node({ mode: "standalone" }), // Si no está en Vercel, usa Node
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
 });
