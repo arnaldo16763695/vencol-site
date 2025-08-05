2c8f2ef03588043ad9d2cf184936400b1da59878
Arnaldo Espinoza <arnaldoespinoza1@hotmail.com>
2025-08-04 20:35:14 -0400

    new astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 import vercel from "@astrojs/vercel";
 
 const isVercel = process.env.VERCEL === "1";
+
 export default defineConfig({
   integrations: [tailwind()],
-  output: isVercel ? "server" : "server", // puedes mantenerlo 'server' en ambos casos
-  adapter: isVercel
-    ? vercel() // Si está en Vercel, usa el adaptador de Vercel
-    : node({ mode: "standalone" }), // Si no está en Vercel, usa Node
+  output: "static", // siempre generamos un sitio estático
+  adapter: isVercel ? vercel() : undefined, // Vercel usa su adaptador, Dokploy no necesita
 });
